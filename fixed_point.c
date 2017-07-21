4dfaef29c0f483a09aa792a77d41eb639c73729a
mjmalek218 <mjmalek218@gmail.com>
2017-07-21 13:59:23 -0500

    first real opening commit. a more defined skeleton has been written in fixed_point.h

---
+++ fixed_point.c
@@ -0,0 +1,28 @@
+#include fixed_point.h
+
+
+/* 
+
+BASED UPON
+
+code.google.com/archive/p/libfixmath/source/default/source
+
+
+--DO NOT INCLUDE COMPLEX NUMBERS
+
+--CREATE README DOCUMENTATION
+
+--UNDERFLOW/OVERFLOW DETECTION
+
+--HARDCODE CERTAIN CONSTANTS
+
+*/
+
+
+
+/* Contains operations for fixed_point arithmetic. 
+
+  Just implement the damn basics. 
+
+ */
+
